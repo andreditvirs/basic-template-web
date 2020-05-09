2c2f3a73b1297fe6ce8e1509e488cb07dfb779c4
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-05-09 22:47:39 +0700

    ONG:query_sql.php 2

--- PHP/query_sql.php
+++ PHP/query_sql.php
@@ -1,5 +1,11 @@
-<!-- mysql_query
+<!-- mysql_query()
     digunakan untuk menjalankan query pada database SQL -->
 <?php
     mysql_query("SELECT * FROM tKomentar");
+?>
+
+<!-- mysql_connect()
+    digunakan untuk menghubungkan file php dengan database MySQL -->
+<?php
+    mysql_connect("localhost", "root", "123456");
 ?>
