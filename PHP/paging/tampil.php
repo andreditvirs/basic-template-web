02e17ae9abef0282c593089f093bb5f969682046
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-05-23 06:56:27 +0700

    ONG:tampil.php 2

--- PHP/paging/tampil.php
+++ PHP/paging/tampil.php
@@ -2,5 +2,10 @@
 mysql_connect("localhost", "root", "");
 mysql_select_db("trikphp");
 
+include "fungsi_paging.php";
+echo "<h2>Daftar kelas</h2>
+	<table border='1'>
+	<tr><th>No</th><th>Nama></th><th>JK</th><th>Alamat</th></tr>
+"
 
 ?>
