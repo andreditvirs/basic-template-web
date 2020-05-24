f9404b6bc5e2fb42ec0065e32076821f0ca90019
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-05-24 15:49:59 +0700

    ONG:terusan commit

--- PHP/paging/tampil.php
+++ PHP/paging/tampil.php
@@ -5,7 +5,25 @@ mysql_select_db("trikphp");
 include "fungsi_paging.php";
 echo "<h2>Daftar kelas</h2>
 	<table border='1'>
-	<tr><th>No</th><th>Nama></th><th>JK</th><th>Alamat</th></tr>
-"
+	<tr><th>No</th><th>Nama></th><th>JK</th><th>Alamat</th></tr>";
+	$p = new Paging;
+	$batas = 5;
+	$posisi = $p->cariPosisi($batas);
+	$tampil = mysql_query("SELECT * FROM mahasiswa LIMIT $posisi, $batas");
+	$no = $posisi+1;
+	while($r = mysql_fetch_array($tampil)){
+		echo "<tr>
+				<td>$no</td>
+				<td>$r[nama]</td>
+				<td>$r[jk]</td>
+				<td>$r[alamat]</td>	
+			</tr>";
+			$no++;
+	}
+echo"</table>";
+$jmldata = mysql_num_rows(mysql_query("SELECT * FROM mahasiswa"));
+$jmlhalaman = $p->jumlahHalaman($jmldata, $batas);
+$linkHalaman = $p->navHalaman($_GET[halaman], $jmlhalaman);
 
+echo "$linkHalaman";
 ?>
